8fe5f1d4e5dd14ac17207de2c0a15edc689e37f7
meddaz <40867923+meddaz@users.noreply.github.com>
2021-12-10 12:58:44 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+onSwitch.addEventListener('click', on);
+offSwitch.addEventListener('click', off);
+
+function on() {     
+document.body.style.backgroundColor = "#eee";
+  
+}
+
+function off() {
+  document.body.style.backgroundColor = "#222";
+
+}
